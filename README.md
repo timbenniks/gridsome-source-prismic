3701e0f7e4bbf4165f6ae4509076747c6f68b5c4
Tim Benniks <tim.benniks@valtech.com>
2020-05-16 15:19:38 +0200

    minor updates to readme and cleanup

---
+++ README.md
@@ -0,0 +1,6 @@
+#Gridsome Source Prismic
+
+Gridsome Source Plugin for Prismic tailored to my personal blog.
+Do not use as a general plugin, take ideas from this for your own.
+
+Based on: ilrock/gridsome-source-prismic
--- utils.js
+++ utils.js
@@ -1,21 +1,16 @@
-// TDOO: handle body field for slices
-
 const DOM = require("prismic-dom")
 
-// fields that don't need any parsing
 const SIMPLE_FIELDS = {
   string: 'string',
   boolean: 'boolean',
   number: 'number'
 }
 
-// supported link types. For now, all but document links
 const LINK_TYPES = {
   media: 'media',
   web: 'web'
 }
 
-// all parser types
 const PARSER_TYPES = {
   NONE: 'none',
   TEXT: 'text',
@@ -23,7 +18,6 @@ const PARSER_TYPES = {
   NOT_SUPPORTED: 'not_supported'
 }
 
-// all header types
 const HEADERS = {
   heading1: 'heading1',
   heading2: 'heading2',
@@ -33,7 +27,6 @@ const HEADERS = {
   heading6: 'heading6'
 }
 
-// function to get the parser type for each field
 const getParser = (field) => {
   if (field) {
     if (SIMPLE_FIELDS[typeof field]) {
@@ -41,9 +34,6 @@ const getParser = (field) => {
     }
 
     if (typeof field === 'object') {
-      // the only array types are headers and rich texts
-      // if there's just one item in the array, then we have
-      // either markdown text or a header
       if (Array.isArray(field)) {
         if (field.length === 1) {
           if (HEADERS[field[0].type]) {
@@ -53,8 +43,6 @@ const getParser = (field) => {
 
         return PARSER_TYPES.HTML
       } else {
-        // the only (supported) object types are images and links
-        // images and links don't need any parsing as they are simple objects
         if (field.dimensions || (field.link_type && LINK_TYPES[field.link_type.toLowerCase()])) {
           return PARSER_TYPES.NONE
         }
@@ -73,7 +61,6 @@ const parseSocialCard = (content) => {
   }
 }
 
-// function that parses a prismic document to a javascript object
 const documentParser = ({
   uid,
   type,
@@ -130,7 +117,6 @@ const documentParser = ({
   return parsedDocument
 }
 
-// function to capitalize words
 const capitalize = (s) => {
   if (typeof s !== 'string') return ''
   return s.charAt(0).toUpperCase() + s.slice(1)
